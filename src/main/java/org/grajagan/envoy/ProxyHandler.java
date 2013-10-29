9148eecb120a7f88772de514bb9c771337b2703b
Helge Weissig <helgew@grajagan.org>
2013-10-29 13:06:44 -0700

    resuccitated CompressionUtils; code cleanup and refactoring

--- src/main/java/org/grajagan/envoy/ProxyHandler.java
+++ src/main/java/org/grajagan/envoy/ProxyHandler.java
@@ -1,171 +1,158 @@
 package org.grajagan.envoy;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.MalformedURLException;
 import java.net.SocketTimeoutException;
 import java.net.URI;
 import java.net.URL;
 import java.net.URLDecoder;
 import java.util.List;
 import java.util.Map.Entry;
-import java.util.zip.DeflaterOutputStream;
-import java.util.zip.InflaterInputStream;
+import java.util.zip.DataFormatException;
 
 import javax.net.ssl.HttpsURLConnection;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
+import org.grajagan.http.HttpHeaders;
 import org.grajagan.ssl.HttpsServerFactory;
 import org.grajagan.ssl.HttpsURLConnectionFactory;
+import org.grajagan.zlib.CompressionUtils;
 
-import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpsServer;
 
 public class ProxyHandler implements HttpHandler {
 
-	public static final int TIMEOUT = 20000;
+    public static final int TIMEOUT = 20000;
     private final URL remoteUrl;
-	private File temporaryDirectory;
-	private static final String prefix = "proxy-";
+    private File temporaryDirectory;
+    private static final String prefix = "proxy-";
 
-	private static final Logger LOG = Logger.getLogger(ProxyHandler.class);
+    private static final Logger LOG = Logger.getLogger(ProxyHandler.class);
 
-	public ProxyHandler(URL upstreamUrl) {
-		this.remoteUrl = upstreamUrl;
-		temporaryDirectory = new File("/var/spool/envoy");
-		if (!temporaryDirectory.exists()) {
-			if (!temporaryDirectory.mkdirs()) {
-				LOG.warn("Cannot log to " + temporaryDirectory
-						+ ", resorting to default temporary directory");
-				temporaryDirectory = null;
-			}
-		}
-	}
+    public ProxyHandler(URL upstreamUrl) {
+        this.remoteUrl = upstreamUrl;
+        temporaryDirectory = new File("/var/spool/envoy");
+        if (!temporaryDirectory.exists()) {
+            if (!temporaryDirectory.mkdirs()) {
+                LOG.warn("Cannot log to " + temporaryDirectory
+                        + ", resorting to default temporary directory");
+                temporaryDirectory = null;
+            }
+        }
+    }
 
-	@Override
-	public void handle(HttpExchange clientExchange) throws IOException {
-		LOG.debug("Received request for " + clientExchange.getRequestURI());
+    @Override
+    public void handle(HttpExchange clientExchange) throws IOException {
+        LOG.debug("Received request for " + clientExchange.getRequestURI());
 
-		File requestFile = File.createTempFile(prefix, ".request", temporaryDirectory);
-		File responseFile = File.createTempFile(prefix, ".response", temporaryDirectory);
+        HttpsURLConnection remoteConnection = getRemoteConnection(clientExchange);
 
-		HttpsURLConnection remoteConnection = getRemoteConnection(clientExchange);
-		
-		FileOutputStream fos = new FileOutputStream(requestFile);
+        // copy client request headers to remote request
+        HttpHeaders requestHeaders = new HttpHeaders(clientExchange.getRequestHeaders());
+        for (Entry<String, List<String>> entry : requestHeaders.entrySet()) {
+            String key = entry.getKey();
+            for (String value : entry.getValue()) {
+                remoteConnection.setRequestProperty(key, value);
+            }
+        }
 
-		Headers headers = clientExchange.getRequestHeaders();
+        // copy client request to array in order to save even if connection times out
+        InputStream is = clientExchange.getRequestBody();
+        byte[] requestBytes = ccopy(is, null);
 
-		boolean isDeflated = false;
+        File requestFile = File.createTempFile(prefix, ".request", temporaryDirectory);
+        String requestString =
+                pickle(requestFile, clientExchange.getRequestMethod() + " "
+                        + remoteConnection.getURL().toString(), requestHeaders, requestBytes);
 
-		StringBuffer hsb = new StringBuffer();
-		for (Entry<String, List<String>> entry : headers.entrySet()) {
-			String key = entry.getKey();
-			for (String value : entry.getValue()) {
-				hsb.append(key + ": " + value + "\n");
-				remoteConnection.setRequestProperty(key, value);
-				if (key.equalsIgnoreCase("content-type")
-						&& value.toLowerCase()
-								.contains("application/x-deflate")) {
-					isDeflated = true;
-				}
-			}
-		}
+        saveXML(requestString);
 
-		hsb.append("\n");
+        OutputStream os = null;
+        try {
+            os = remoteConnection.getOutputStream();
+        } catch (SocketTimeoutException e) {
+            LOG.warn("timeout while trying to access remote host");
+            clientExchange.close();
+            return;
+        }
 
-		LOG.debug("writing request to " + requestFile);
-		IOUtils.write(clientExchange.getRequestMethod() + " "
-				+ remoteConnection.getURL().toString() + "\n\n", fos);
-		IOUtils.write(hsb.toString(), fos);
+        // now send the client request to the remote
+        IOUtils.write(requestBytes, os);
+        os.close();
 
-		InputStream is = clientExchange.getRequestBody();
-		if (isDeflated) {
-			is = new InflaterInputStream(is);
-		}
-		String requestString = IOUtils.toString(is);
-		IOUtils.write(requestString, fos);
-		fos.close();
+        // copy remote response headers to client response
+        HttpHeaders responseHeaders = new HttpHeaders(remoteConnection.getHeaderFields());
+        clientExchange.getResponseHeaders().putAll(responseHeaders);
 
-		saveXML(requestString);
+        // send the headers to the client
+        clientExchange.sendResponseHeaders(remoteConnection.getResponseCode(), responseHeaders
+                .getContentLength());
 
-		OutputStream os = null;
-		try {
-			os = remoteConnection.getOutputStream();
-		} catch (SocketTimeoutException e) {
-			LOG.warn("timeout while trying to access remote host");
-			clientExchange.close();
-			responseFile.delete();
-			return;
-		}
+        // copy remote response to client
+        is = remoteConnection.getInputStream();
+        os = clientExchange.getResponseBody();
+        byte[] responseBytes = ccopy(is, os);
 
-		if (isDeflated) {
-			os = new DeflaterOutputStream(os);
-		}
-		IOUtils.write(requestString, os);
-		os.close();
+        File responseFile = File.createTempFile(prefix, ".response", temporaryDirectory);
+        pickle(responseFile, responseHeaders.getFirst(null), responseHeaders, responseBytes);
 
-		int statusCode = remoteConnection.getResponseCode();
+        os.close();
+        clientExchange.close();
+    }
 
-		headers = clientExchange.getResponseHeaders();
-		String responseMessage = null;
-		hsb = new StringBuffer();
-		isDeflated = false;
-		for (Entry<String, List<String>> entry : remoteConnection
-				.getHeaderFields().entrySet()) {
-			String key = entry.getKey();
-			for (String value : entry.getValue()) {
-				if (key != null) {
-					hsb.append(key + ": " + value + "\n");
-					headers.add(key, value);
-					if (key.equalsIgnoreCase("content-type")
-							&& value.toLowerCase().contains(
-									"application/x-deflate")) {
-						isDeflated = true;
-					}
-				} else {
-					responseMessage = value;
-				}
-			}
-		}
+    private String pickle(File file, String method, HttpHeaders headers, byte[] body)
+            throws IOException {
+        FileOutputStream fos = new FileOutputStream(file);
 
-		hsb.append("\n");
-		LOG.debug("writing response to " + responseFile);
-		fos = new FileOutputStream(responseFile);
-		IOUtils.write(responseMessage + "\n" + hsb.toString(), fos);
+        LOG.debug("writing to " + file);
+        IOUtils.write(method + "\n\n", fos);
+        IOUtils.write(headers.toString() + "\n", fos);
 
-		is = remoteConnection.getInputStream();
-		if (isDeflated) {
-			is = new InflaterInputStream(is);
-		}
+        byte[] bytes = body;
+        if (headers.isDeflated()) {
+            try {
+                bytes = CompressionUtils.decompress(body);
+            } catch (DataFormatException e) {
+                LOG.warn("Cannot decompress data", e);
+            }
+        }
 
-		String responseString = IOUtils.toString(is);
-		IOUtils.write(responseString, fos);
-		fos.close();
+        String string = new String(bytes);
+        IOUtils.write(string, fos);
+        fos.close();
+        return string;
+    }
 
-		clientExchange.sendResponseHeaders(statusCode, responseString.length());
-		os = clientExchange.getResponseBody();
-		if (isDeflated) {
-			os = new DeflaterOutputStream(os);
-		}
-		IOUtils.write(responseString, os);
+    private static byte[] ccopy(InputStream input, OutputStream output) throws IOException {
+        int n = 0;
+        byte[] buffer = new byte[4096];
+        ByteArrayOutputStream ccos = new ByteArrayOutputStream();
+        while (-1 != (n = input.read(buffer))) {
+            if (output != null) {
+                output.write(buffer, 0, n);
+            }
+            ccos.write(buffer, 0, n);
+        }
+        LOG.debug("CCopied " + ccos.size() + " bytes ");
+        return ccos.toByteArray();
+    }
 
-		clientExchange.close();
-	}
-
-	protected HttpsURLConnection getRemoteConnection(HttpExchange clientExchange) throws IOException {
+    protected HttpsURLConnection getRemoteConnection(HttpExchange clientExchange)
+            throws IOException {
         HttpsURLConnection remoteConnection = null;
-        URL url = new URL(remoteUrl.getProtocol(), remoteUrl.getHost(),
-                remoteUrl.getPort(), clientExchange.getRequestURI().toString());
+        URL url =
+                new URL(remoteUrl.getProtocol(), remoteUrl.getHost(), remoteUrl.getPort(),
+                        clientExchange.getRequestURI().toString());
 
         try {
-            remoteConnection = HttpsURLConnectionFactory
-                    .createHttpsURLConnection(url);
+            remoteConnection = HttpsURLConnectionFactory.createHttpsURLConnection(url);
         } catch (Exception e) {
             LOG.error("Cannot create remote connection", e);
             throw new IOException(e);
@@ -179,21 +166,22 @@ public class ProxyHandler implements HttpHandler {
     }
 
     private void saveXML(String requestString) throws IOException {
-		File xml = File.createTempFile(prefix, ".xml", temporaryDirectory);
-		for (String keyValue : requestString.split("&")) {
-			String[] pair = keyValue.split("=");
-			if (pair[0].equals("body")) {
-				LOG.debug("writing xml to " + xml);
-				IOUtils.write(URLDecoder.decode(pair[1], "UTF-8"),
-						new FileOutputStream(xml));
-				return;
-			}
-		}
-		LOG.warn("no body key found");
-		xml.delete();
-	}
+        File xml = File.createTempFile(prefix, ".xml", temporaryDirectory);
+        for (String keyValue : requestString.split("&")) {
+            String[] pair = keyValue.split("=");
+            if (pair[0].equals("body")) {
+                LOG.debug("writing xml to " + xml);
+                FileOutputStream fos = new FileOutputStream(xml);
+                IOUtils.write(URLDecoder.decode(pair[1], "UTF-8"), fos);
+                fos.close();
+                return;
+            }
+        }
+        LOG.warn("no body key found");
+        xml.delete();
+    }
 
-	public File getTemporaryDirectory() {
+    public File getTemporaryDirectory() {
         return temporaryDirectory;
     }
 
@@ -202,38 +190,37 @@ public class ProxyHandler implements HttpHandler {
     }
 
     /**
-	 * Main method to debug connections to the server on a given port.
-	 * 
-	 * @param argv
-	 *            the port to connect to as a number
-	 * @throws Exception
-	 *             if any errors occur
-	 */
-	public static void main(String[] argv) throws Exception {
-		final int defaultPort = 7777;
-		int port = defaultPort;
-		String remote = "https://mail.grajagan.org";
-		String host = "localhost";
+     * Main method to debug connections to the server on a given port.
+     * 
+     * @param argv
+     *            the port to connect to as a number
+     * @throws Exception
+     *             if any errors occur
+     */
+    public static void main(String[] argv) throws Exception {
+        final int defaultPort = 7777;
+        int port = defaultPort;
+        String remote = "https://mail.grajagan.org";
+        String host = "localhost";
 
-		if (argv.length > 0) {
-			remote = argv[0];
-		}
+        if (argv.length > 0) {
+            remote = argv[0];
+        }
 
-		if (argv.length > 1) {
-			host = argv[1];
-		}
+        if (argv.length > 1) {
+            host = argv[1];
+        }
 
-		if (argv.length > 2) {
-			port = Integer.parseInt(argv[2]);
-		}
+        if (argv.length > 2) {
+            port = Integer.parseInt(argv[2]);
+        }
 
-		LOG.debug("Starting proxy for " + remote + " on " + host + " and port "
-				+ port);
-		HttpsServer server = HttpsServerFactory.createServer(host, port);
-		URL url = URI.create(remote).toURL();
-		server.createContext("/", new ProxyHandler(url));
-		server.start();
-		while (true) {
-		}
-	}
+        LOG.debug("Starting proxy for " + remote + " on " + host + " and port " + port);
+        HttpsServer server = HttpsServerFactory.createServer(host, port);
+        URL url = URI.create(remote).toURL();
+        server.createContext("/", new ProxyHandler(url));
+        server.start();
+        while (true) {
+        }
+    }
 }
---
+++ src/main/java/org/grajagan/http/HttpHeaders.java
@@ -0,0 +1,61 @@
+package org.grajagan.http;
+
+import java.util.List;
+import java.util.Map;
+
+import com.sun.net.httpserver.Headers;
+
+public class HttpHeaders extends Headers {
+
+    public HttpHeaders(Map<String, List<String>> headers) {
+        super();
+        putAll(headers);
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer hsb = new StringBuffer();
+        for (Entry<String, List<String>> entry : this.entrySet()) {
+            String key = entry.getKey();
+            if (key != null) {
+                for (String value : entry.getValue()) {
+                    hsb.append(key + ": " + value + "\n");
+                }
+            }
+        }
+        return hsb.toString();
+    }
+
+    public int getContentLength() {
+        for (Entry<String, List<String>> entry : this.entrySet()) {
+            String key = entry.getKey();
+            for (String value : entry.getValue()) {
+                if (key.equalsIgnoreCase("content-length")) {
+                    return Integer.parseInt(value);
+                }
+            }
+        }
+        return 0;
+    }
+
+    public void setContentLength(int contentLength) {
+        this.set("Content-Length", String.valueOf(contentLength));
+    }
+
+    public boolean isDeflated() {
+        for (Entry<String, List<String>> entry : this.entrySet()) {
+            String key = entry.getKey();
+            for (String value : entry.getValue()) {
+                if (key.equalsIgnoreCase("content-type")
+                        && value.toLowerCase().contains("application/x-deflate")) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void setDeflated(boolean isDeflated) {
+        this.set("Content-Type", "application/x-deflated");
+    }
+}
