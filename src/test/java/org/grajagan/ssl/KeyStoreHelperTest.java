f657fa2b1517722677b775ed8ca439add3c7d57d
Helge Weissig <helgew@grajagan.org>
2014-06-11 12:31:13 -0700

    added test class

---
+++ src/test/java/org/grajagan/ssl/KeyStoreHelperTest.java
@@ -0,0 +1,34 @@
+package org.grajagan.ssl;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.TrustManagerFactory;
+
+import org.junit.Test;
+
+public class KeyStoreHelperTest {
+
+    @Test
+    public void testGetKeyStore() throws KeyStoreException {
+        KeyStore ks = KeyStoreHelper.getKeyStore();
+        assertNotNull(ks);
+    }
+
+    @Test
+    public void testGetKeyManagerFactory() throws KeyStoreException {
+        KeyManagerFactory kmf = KeyStoreHelper.getKeyManagerFactory();
+        assertNotNull(kmf);
+    }
+
+    @Test
+    public void testGetTrustManagerFactory() throws KeyStoreException {
+        TrustManagerFactory tmf = KeyStoreHelper.getTrustManagerFactory();
+        assertNotNull(tmf);
+    }
+
+}
