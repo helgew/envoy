62751a8380203f1765edc0bb3960df7c41c77517
Helge Weissig <helgew@grajagan.org>
2017-02-09 15:39:37 -0800

    Revert "another InfluxDB schema change" - very slow query performance
    This reverts commit 3ff376f00c17b0f5c11bd38a2a1eacc7b9e14ac2.

--- src/test/java/org/grajagan/envoy/influxdb/InfluxDBLoaderTest.java
+++ src/test/java/org/grajagan/envoy/influxdb/InfluxDBLoaderTest.java
@@ -33,13 +33,12 @@ public class InfluxDBLoaderTest {
             Reading reading = ReadingHelper.parseFromXmlElement(readXML);
             reading.setEquipmentId(1);
             Point point = loader.createPoint(reading);
-            assertEquals("reading " +
+            assertEquals("reading,panel=panel1 " +
                     "ac_frequency=" + reading.getAcFrequency() + "," +
                     "ac_voltage=" + reading.getAcVoltage() + "," +
                     "dc_current=" + reading.getDcCurrent() + "," +
                     "dc_voltage=" + reading.getDcVoltage() + "," +
                     "duration=" + reading.getDuration() + "i," +
-                    "panel=\"panel1\"," +
                     "temperature=" + reading.getTemperature() + "i," +
                     "watt_seconds=" + reading.getWattSeconds() + "i " +
                     reading.getDate().getTime() * 1000000, point.lineProtocol());
